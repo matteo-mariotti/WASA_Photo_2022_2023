4728215c5c24f0d9b392ff3940a790a0c803bdb6
Matteo-mariotti <mariotti.1936936@studenti.uniroma1.it>
2022-12-20 20:17:58 +0100

    Photo upload completed

--- webui/src/components/UploadBox.vue
+++ webui/src/components/UploadBox.vue
@@ -1,12 +1,16 @@
 <script>
 
 import backend from "@/services/axios";
+import SuccessMsg from "@/components/SuccessMsg.vue";
+import ErrorMsg from "@/components/ErrorMsg.vue";
 
 export default {
-
+  components: {ErrorMsg, SuccessMsg},
   data: function () {
     return {
       file: null,
+      successmsg: null,
+      errormsg: null,
     }
   },
   methods: {
@@ -14,14 +18,20 @@ export default {
       this.file = this.$refs.file.files[0];
       console.log(this.file)
     },
+    reload(){
+      location.reload()
+    },
     async submitFile() {
       try {
         let formData = new FormData()
         formData.append("file", this.file)
 
         await backend.post(`/users/${sessionStorage.getItem("username")}/photos`,formData)
-
+        this.errormsg = null
+        this.successmsg = "Photo successfully uploaded"
+        setTimeout(this.reload, 1000)
       }catch(error){
+        this.errormsg = "Error while uploading photo"
         console.log(error)
       }
     },
@@ -34,6 +44,7 @@ export default {
 
 <template>
   <div class="container">
+
     <div class="text-center">
       <h5>Choose a photo to upload</h5>
       <hr/>
@@ -42,6 +53,8 @@ export default {
       <br>
       <button class="btn btn-primary">Upload</button>
       </form>
+      <SuccessMsg :msg="successmsg" v-if="successmsg" class="mt-2"></SuccessMsg>
+      <ErrorMsg :msg="errormsg" v-if="errormsg" class="mt-2"></ErrorMsg>
     </div>
   </div>
 </template>
